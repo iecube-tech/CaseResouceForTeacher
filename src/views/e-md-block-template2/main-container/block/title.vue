855ee7ca58e5d261c184a12ff194dc3a063093f9
iecube-zhuxiaoxi <xiaoxi.zhu@iecube.com.cn>
2025-08-07 16:04:26 +0800

    新增编辑问题模块

---
+++ src/views/e-md-block-template2/main-container/block/title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { type PAYLOAD } from '../../../../ts/block';
+import textpreview from '../../../emdV3/textPreview/textPreview.vue'
+const props = defineProps({
+    payload: Object
+})
+
+const payload = ref<PAYLOAD>(<any>props.payload)
+</script>
+<style scoped></style>
